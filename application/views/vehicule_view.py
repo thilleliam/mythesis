fa92aaa1903a42772de7b5cd96b3b19d187fff7a
Thilleli <ameurthilleli2002@gmail.com>
2025-06-04 20:43:15 +0200

    Message clair décrivant les modifications

--- application/views/vehicule_view.py
+++ application/views/vehicule_view.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:204679bbd06ee2026b4215a7c87e42e270e493ad272695c761d53ce37e55aebc
-size 44543
+oid sha256:17dd6461c19b34bcfd6945555611057180bce06916e6505e2c1ba8b98fda6f47
+size 44657
