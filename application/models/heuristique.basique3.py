2e03d0ad96f0432710e63ee47e0dae242258e715
Thilleli <ameurthilleli2002@gmail.com>
2025-06-04 12:18:08 +0200

    Initial commit propre sans fichiers lourds

--- application/models/heuristique.basique3.py
+++ application/models/heuristique.basique3.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d3f5bc503c66de1b844b00a3be5fdd90aa347c7a9a283b41d7ccee81b1b76a4b
-size 93344
+oid sha256:95ce858f12e7868b8450d9961ccd67926b0251f9e9f7a834af6c1ffbe1639708
+size 124162
---
+++ application/models/heuristiqueconsoleparser.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:83f0479fadd61f61eaaa48d1d65bda517501eb64aacf62919264fc3a8e7792a4
+size 24305
