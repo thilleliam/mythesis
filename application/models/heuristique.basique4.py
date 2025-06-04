5d2c5f1246080f18a9a06d745ff5000121170d80
Thilleli <ameurthilleli2002@gmail.com>
2025-06-04 09:15:33 +0200

    Initial commit propre sans fichiers lourds

--- application/models/heuristique.basique4.py
+++ application/models/heuristique.basique4.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0eb1c7afbc059b86f7f17b093a30ef8c127da13b6c08568f2ea0c1ad8c0c667d
-size 101357
+oid sha256:d6d4b946d5a30b0722256b03d2c58979a4d10804d28e4efc7fcbba21459fe2f6
+size 104815
