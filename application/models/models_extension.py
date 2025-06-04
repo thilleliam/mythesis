a279ccddcb1c381a9ec354e4645387e33eb7959c
Thilleli <ameurthilleli2002@gmail.com>
2025-06-04 08:54:47 +0200

    Initial commit propre sans fichiers lourds

--- application/models/models_extension.py
+++ application/models/models_extension.py
@@ -1,17 +1,3 @@
-from application.models import TransfererEquipement, Equipement
-
-def get_equipement(self, session):
-    """Méthode pour TransfererEquipement"""
-    return session.query(Equipement).filter(
-        Equipement.nomEquipement == self.designation_equipement
-    ).first()
-
-def get_transferts(self, session):
-    """Méthode pour Equipement"""
-    return session.query(TransfererEquipement).filter(
-        TransfererEquipement.designation_equipement == self.nomEquipement
-    ).all()
-
-# Ajout des méthodes aux classes
-TransfererEquipement.get_equipement = get_equipement
-Equipement.get_transferts = get_transferts
+version https://git-lfs.github.com/spec/v1
+oid sha256:907ddf5e32a7de974094e48249d558194665c262a2fc1032182501328d966673
+size 604
