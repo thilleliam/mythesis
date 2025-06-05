e68f0f0fee8ce55ce0d4935b71454dda037df546
Thilleli <ameurthilleli2002@gmail.com>
2025-06-06 00:50:32 +0200

    Message clair sur les modifications

--- application/models/heuristique.py
+++ application/models/heuristique.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:10eadfb6847d6efe03f85514502fb1a3091190741764581eb269c5b6eb1cc684
-size 204556
+oid sha256:39aa68dc893fa34dfa534f60db8d07437132dccd016030e015c9fe2a620ee0f9
+size 234784
