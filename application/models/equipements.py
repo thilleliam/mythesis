791a42af3f59a6a731fdb5bcc334f1f95860df64
Thilleli <ameurthilleli2002@gmail.com>
2025-06-06 21:41:47 +0200

    Ajout de la page de gestion des utilisateurs

--- application/models/equipements.py
+++ application/models/equipements.py
@@ -1,15 +1,3 @@
-from sqlalchemy import Column, ForeignKey , Float, Integer, String
-from application.database import Base
-from sqlalchemy.orm import relationship , backref
-class Equipement(Base):
-    __tablename__ = "equipements"
-
-    ID_equipement = Column(Integer, primary_key=True, autoincrement=True)
-    quantite = Column(Integer)
-    nomEquipement = Column(String(255))
-    typeEquipement = Column(String(255))
-    etat = Column(String(255))
-    poids = Column(Float, nullable=True, comment="Poids de l'équipement en kilogrammes")
-    volume = Column(Float, nullable=True, comment="Volume de l'équipement en mètres cubes")
-commandes = relationship("CommandeEquipement")
-transferer_equipements = relationship("TransfererEquipement", backref=backref("equipement", lazy="joined"))
+version https://git-lfs.github.com/spec/v1
+oid sha256:f9b77c5d0bf8f9cf1b0f25e956e89faeff4e59bbdca5a8183b7b316823408c26
+size 769
