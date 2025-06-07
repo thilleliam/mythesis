cf98a63c245ab1fee7344a4114e6a7e69dcc15a6
Thilleli <ameurthilleli2002@gmail.com>
2025-06-07 13:32:09 +0200

    Message clair sur les modifications

--- application/api/api/position_conducteur.py
+++ application/api/api/position_conducteur.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d793980205972c4e730f1b573e5d9e6b45816fa88f14393cf292a1701e678357
-size 59500
+oid sha256:cecb339f3fd1303e02ee7cec3a20796c892150decf62f687a56ac6b14d436c90
+size 56302
