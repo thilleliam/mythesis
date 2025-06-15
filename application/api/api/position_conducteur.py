cf7a2ef7a00d6eabe10f8bb528824bc5bae96e42
Thilleli <ameurthilleli2002@gmail.com>
2025-06-15 06:03:28 +0200

    Fix: Correction manifeste PWA et ajout routes debug pour Render

--- application/api/api/position_conducteur.py
+++ application/api/api/position_conducteur.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:95e20dd3fa59c6af87568b68bd9c8953659f9d3825f1ab0fb37b8abef87d9b81
-size 71716
+oid sha256:7ec2430c12a4317f7421c87122dd46baa87d3a24db7f1afb7a236a7075b6b5e2
+size 87235
