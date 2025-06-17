6b6635e6e6cf371fc24210ec46facd153ab79e39
Thilleli <ameurthilleli2002@gmail.com>
2025-06-17 07:32:27 +0200

    Uniformisation des couleurs des cadres de tableaux de bord avec la couleur bleue info

--- application/main.py
+++ application/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fd10a44b226f4c2345544340da2de3ddce714489c355b7bd6023ea3e978a16cc
-size 109698
+oid sha256:d29542ad29216d377c401be0155ce7d41b86a73b06f4c947d91cedd2586566fc
+size 110587
