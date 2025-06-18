244f6009dc6d51e77cafc8add7cdd4fe64855d23
Thilleli <ameurthilleli2002@gmail.com>
2025-06-18 12:11:01 +0200

    Ton message de commit

--- application/api/api/commande_api.py
+++ application/api/api/commande_api.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:609d5b47360b9ecdf596df9733122a88e120b2cfedcc8c57ebd5a95ea198d095
-size 36617
+oid sha256:8694b113f109cda2a42aadb2f7b26504fff44db7ea04b328e92fea8ffdf657a6
+size 50551
--- application/models/heuristique.basique3.py
+++ application/models/heuristique.basique3.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e6364c2f39b06ad9f9f490ba01d961084cf6cdb29aad6741a46d228c104c602f
-size 211180
+oid sha256:2de44d6718f3dff1e520f96c924949f266b269cccf73cded4abd13cc32b92d8e
+size 211182
